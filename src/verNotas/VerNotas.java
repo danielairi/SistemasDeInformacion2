c8090c7ac01f36b5a304f438ce06a86135860e4f
JoDemVel <62674855+JoDemVel@users.noreply.github.com>
2021-05-01 14:31:40 -0400

    Add files via upload

--- src/verNotas/VerNotas.java
+++ src/verNotas/VerNotas.java
@@ -1,12 +1,27 @@
-package verNotas.MostrarNotasGestion;
+package verNotas;
+
+import Conexion.ConexionBD;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
 
 public class VerNotas extends javax.swing.JFrame {
-
     
-    public VerNotas() {
+    private String gestion;
+    private String codSis;
+    
+    public VerNotas(String gestion) {
+        this.gestion = gestion;
+        
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.setResizable(false);
         initComponents();
+        mostrarNota();
     }
 
     @SuppressWarnings("unchecked")
@@ -14,20 +29,145 @@ public class VerNotas extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jTextFieldGestion = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
+        jTextFieldPromedio = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jTextFieldApr = new javax.swing.JTextField();
+        jTextFieldRep = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(182, 208, 225));
 
+        jTextFieldGestion.setEditable(false);
+        jTextFieldGestion.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
+
+        jLabel1.setBackground(new java.awt.Color(26, 69, 104));
+        jLabel1.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+        jLabel1.setText("GESTION:");
+
+        jTable1.setBackground(new java.awt.Color(240, 240, 240));
+        jTable1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Materia", "Sigla", "Gestion", "Calificacion"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Integer.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jTable1.setSelectionForeground(new java.awt.Color(182, 208, 225));
+        jScrollPane1.setViewportView(jTable1);
+        if (jTable1.getColumnModel().getColumnCount() > 0) {
+            jTable1.getColumnModel().getColumn(0).setResizable(false);
+            jTable1.getColumnModel().getColumn(0).setPreferredWidth(200);
+            jTable1.getColumnModel().getColumn(1).setResizable(false);
+            jTable1.getColumnModel().getColumn(2).setResizable(false);
+            jTable1.getColumnModel().getColumn(3).setResizable(false);
+        }
+        jTable1.getAccessibleContext().setAccessibleParent(null);
+
+        jLabel2.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+        jLabel2.setText("PROMEDIO:");
+
+        jTextFieldPromedio.setEditable(false);
+        jTextFieldPromedio.setBackground(new java.awt.Color(144, 175, 196));
+        jTextFieldPromedio.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+
+        jLabel3.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+        jLabel3.setText("Materias Aprobadas:");
+
+        jTextFieldApr.setEditable(false);
+        jTextFieldApr.setBackground(new java.awt.Color(144, 175, 196));
+        jTextFieldApr.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+
+        jTextFieldRep.setEditable(false);
+        jTextFieldRep.setBackground(new java.awt.Color(144, 175, 196));
+        jTextFieldRep.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+
+        jLabel4.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+        jLabel4.setText("Materias Reprobadas:");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 650, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(90, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTextFieldGestion, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(196, 196, 196))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 480, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(80, 80, 80))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(99, 99, 99)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel4))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTextFieldRep, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextFieldPromedio, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextFieldApr, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 650, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(152, 152, 152)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextFieldGestion, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(81, 81, 81)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(58, 58, 58)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextFieldPromedio, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jTextFieldApr, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextFieldRep, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addContainerGap(37, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -73,12 +213,65 @@ public class VerNotas extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new VerNotas().setVisible(true);
+                new VerNotas("II-2020").setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField jTextFieldApr;
+    private javax.swing.JTextField jTextFieldGestion;
+    private javax.swing.JTextField jTextFieldPromedio;
+    private javax.swing.JTextField jTextFieldRep;
     // End of variables declaration//GEN-END:variables
+
+    private void mostrarNota() {
+        try {
+            codSis = Ingresar.getCodSis();
+            
+            jTextFieldGestion.setText(this.gestion);
+            DefaultTableModel modelo = (DefaultTableModel)jTable1.getModel();
+            modelo.setRowCount(0); 
+            ConexionBD cs = new ConexionBD();
+            Connection cBD = cs.conexion();
+            Statement instruccion = cBD.createStatement();
+            ResultSet rs = instruccion.executeQuery("select nombreMat,Materia.sigla,gestion,calif\n" +
+                                                    "from Materia, Nota\n" +
+                                                    "where Nota.sigla = Materia.sigla and gestion = '"+gestion+"'"+"and Nota.codSis="+codSis);
+            Vector v = new Vector();
+            int cant=0, total=0, matApr = 0, matRep = 0;
+            try{
+                while(rs.next()){
+                    v.add(rs.getString(1));
+                    v.add(rs.getString(2));
+                    v.add(rs.getString(3));
+                    int calif = rs.getInt(4);
+                    if(calif>=51) matApr++;
+                    else matRep++;
+                    v.add(calif);
+                    modelo.addRow(v);
+                    cant++; total = total+calif;
+                    v = new Vector();
+                }
+                jTable1.setModel(modelo);
+                jTextFieldPromedio.setText(((double)total/cant)+"");
+                jTextFieldApr.setText(matApr+"");
+                jTextFieldRep.setText(matRep+"");
+            }catch(SQLException e){
+                
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(VerNotas.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(VerNotas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+           
+    }
 }
