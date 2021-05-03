b57f339571a7470219e72d9f3cd162c0d426c3ea
alvaro0000 <83039826+alvaro0000@users.noreply.github.com>
2021-05-02 20:41:08 -0400

    Add files via upload

--- src/verNotas/Ingresar.java
+++ src/verNotas/Ingresar.java
@@ -1,27 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package interfaces;
-
-import PaqueteSQL.Conexion_BD;
-import static PaqueteSQL.Metodos_SQL.conexion;
+package verNotas;
+import Conexion.ConexionBD;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
-
-/**
+/*
  *
  * @author alvaro
  */
 public class Ingresar extends javax.swing.JFrame {
-ConexionBD cs = new ConexionBD();
-Connection cBD = cs.conexion();
+    
     static String dato_usuario;
     /**
      * Creates new form hola2
@@ -46,18 +38,21 @@ Connection cBD = cs.conexion();
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         ingresar = new javax.swing.JButton();
         usuario = new javax.swing.JTextField();
-        contraseña = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        contraseña = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(182, 208, 225));
 
-        jLabel1.setText("Codigo SIS");
+        jPanel1.setBackground(new java.awt.Color(144, 175, 196));
 
-        jLabel2.setText("Contraseña (CI)");
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel2.setText("Contraseña (CI):");
 
         ingresar.setText("ingresar");
         ingresar.addActionListener(new java.awt.event.ActionListener() {
@@ -66,82 +61,103 @@ Connection cBD = cs.conexion();
             }
         });
 
+        usuario.setBackground(new java.awt.Color(203, 222, 237));
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel3.setText("UNIVERSIDAD MAYOR DE SAN SIMON");
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel1.setText("Codigo SIS:");
+
+        contraseña.setBackground(new java.awt.Color(203, 222, 237));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(ingresar)
+                .addGap(222, 222, 222))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(77, 77, 77)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(usuario)
+                .addGap(52, 52, 52))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(49, 49, 49)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(contraseña, javax.swing.GroupLayout.PREFERRED_SIZE, 331, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(112, 112, 112)
+                        .addComponent(jLabel3)))
+                .addContainerGap(50, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(50, 50, 50)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 72, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(usuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(contraseña, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(50, 50, 50)
+                .addComponent(ingresar)
+                .addGap(55, 55, 55))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(116, 116, 116)
-                        .addComponent(jLabel1)
-                        .addGap(44, 44, 44))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel2)
-                        .addGap(29, 29, 29)))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(usuario)
-                    .addComponent(contraseña))
-                .addGap(79, 79, 79))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(228, 228, 228)
-                .addComponent(ingresar)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(183, Short.MAX_VALUE)
-                .addComponent(jLabel3)
-                .addGap(163, 163, 163))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(39, 39, 39)
-                .addComponent(jLabel3)
-                .addGap(40, 40, 40)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(usuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
-                .addGap(34, 34, 34)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(contraseña, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(72, 72, 72)
-                .addComponent(ingresar)
-                .addContainerGap(62, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void ingresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ingresarActionPerformed
-        Calendar calendario = Calendar.getInstance();
-        int hora, minutos, segundos;
-        hora = calendario.get(Calendar.HOUR_OF_DAY);
-        minutos = calendario.get(Calendar.MINUTE);
-        segundos = calendario.get(Calendar.SECOND);
-        System.out.println(hora + ":" + minutos + ":" + segundos);
-        int horaIni = 8;
-        int horaFin = 22;
-        String user = usuario.getText();
-        String pass = contraseña.getText();
-        
-
-        try {
-            String sql = "select * from Estudiante where codSis=" + user + " and ci=" + pass;
-            Statement sent = cnn.createStatement();
-            ResultSet rs = sent.executeQuery(sql);
-            if (rs.next()) {
-                
+        try {                                         
+            ConexionBD cs = new ConexionBD();
+            Connection cBD = cs.conexion();
+            Calendar calendario = Calendar.getInstance();
+            int hora, minutos, segundos;
+            hora = calendario.get(Calendar.HOUR_OF_DAY);
+            minutos = calendario.get(Calendar.MINUTE);
+            segundos = calendario.get(Calendar.SECOND);
+            System.out.println(hora + ":" + minutos + ":" + segundos);
+            int horaIni = 8;
+            int horaFin = 22;
+            String user = usuario.getText();
+            String pass = contraseña.getText();
+            
+            
+            try {
+                String sql = "select * from Estudiante where codSis=" + user + " and ci=" + pass;
+                Statement sent = cBD.createStatement();
+                ResultSet rs = sent.executeQuery(sql);
+                if (rs.next()) {
+                    
                     if (horaIni <= hora && horaFin > hora) {
-                       
-                       DatosEstudiante tip = new DatosEstudiante() ;
+                        codsis =user;
+                        VerNota tip = new VerNota() ;
                         tip.setVisible(true);
                         this.dispose();
                         
-                        codsis =user;
+                        
                     } else {
                         if (hora < horaIni) {
                             JOptionPane.showMessageDialog(null, "El sistema se abre 8 am");
@@ -149,12 +165,17 @@ Connection cBD = cs.conexion();
                             JOptionPane.showMessageDialog(null, "El sistema se cerro 4 pm");
                         }
                     }
-                
-            } else {
-                JOptionPane.showMessageDialog(null, "Usuario o Contraseña Incorrecta");
-            }
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "error: " + e);
+                    
+                } else {
+                    JOptionPane.showMessageDialog(null, "Usuario o Contraseña Incorrecta");
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "error: " + e);
+            }        // TODO add your handling code here:
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Ingresar.class.getName()).log(Level.SEVERE,null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Ingresar.class.getName()).log(Level.SEVERE, null, ex);
         }        // TODO add your handling code here:
     }//GEN-LAST:event_ingresarActionPerformed
     
@@ -197,17 +218,18 @@ Connection cBD = cs.conexion();
         });
     }
     public static String getCodSis(){
-    return codsis;
-}
+        return codsis;
+    }
     
  
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField contraseña;
+    private javax.swing.JPasswordField contraseña;
     private javax.swing.JButton ingresar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField usuario;
     // End of variables declaration//GEN-END:variables
 }
